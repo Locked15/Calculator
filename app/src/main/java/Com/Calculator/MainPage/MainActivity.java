217c47ec03e2f8adf72e25d9541e5da2bfd1f070
Your Name <you@example.com>
2021-09-23 14:11:21 +0300

    Added new feature.

--- app/src/main/java/Com/Calculator/MainPage/MainActivity.java
+++ app/src/main/java/Com/Calculator/MainPage/MainActivity.java
@@ -1,15 +1,34 @@
 package Com.Calculator.MainPage;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Главный класс программы.
  */
 public class MainActivity extends AppCompatActivity
 {
+    /**
+     * Словарь, содержащий числа.
+     */
+    private final Map<View, String> numbers = new HashMap<>(1);
+
+    /**
+     * Словарь, содержащий операторы выражения.
+     */
+    private final Map<View, String> operators = new HashMap<>(1);
+
+    /**
+     * Элемент, содержащий введенное выражение.
+     */
+    private final TextView answerBlock = (TextView)findViewById(R.id.AnswerView);
+
     /**
      * Событие, происходящее при запуске приложения.
      *
@@ -20,17 +39,50 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeDictionaries();
     }
-    
+
+    /**
+     * Метод, нужный для инициализации словарей.
+     */
+    private void initializeDictionaries()
+    {
+        // region Инициализация словаря с числами.
+        numbers.put((View)findViewById(R.id.NumberZero), "0");
+        numbers.put((View)findViewById(R.id.NumberOne), "1");
+        numbers.put((View)findViewById(R.id.NumberTwo), "2");
+        numbers.put((View)findViewById(R.id.NumberThree), "3");
+        numbers.put((View)findViewById(R.id.NumberFour), "4");
+        numbers.put((View)findViewById(R.id.NumberFive), "5");
+        numbers.put((View)findViewById(R.id.NumberSix), "6");
+        numbers.put((View)findViewById(R.id.NumberSeven), "7");
+        numbers.put((View)findViewById(R.id.NumberEight), "8");
+        numbers.put((View)findViewById(R.id.NumberNine), "9");
+        //endregion
+
+        //region Инициализация словаря с операциями.
+        operators.put((View)findViewById(R.id.ActionPlus), "+");
+        operators.put((View)findViewById(R.id.ActionMinus), "-");
+        operators.put((View)findViewById(R.id.ActionMultiply), "*");
+        operators.put((View)findViewById(R.id.ActionDivide), "/");
+        operators.put((View)findViewById(R.id.ActionLeftBrace), "(");
+        operators.put((View)findViewById(R.id.ActionRightBrace), ")");
+        operators.put((View)findViewById(R.id.ActionDegree), "^");
+        //endregion
+    }
+
     /**
      * Событие, происходящее при нажатии на кнопку с цифрой.
      * Добавляет эту цифру в расчет.
      *
      * @param number Элемент, вызвавший событие.
      */
+    @SuppressLint("SetTextI18n")
     public void numberAreClicked(View number)
     {
-    
+        //В идеале, это надо сделать через "StringBuilder":
+        answerBlock.setText(answerBlock.getText() + numbers.get(answerBlock));
     }
     
     /**
@@ -39,9 +91,11 @@ public class MainActivity extends AppCompatActivity
      *
      * @param operation Операция, которая будет добавлена.
      */
+    @SuppressLint("SetTextI18n")
     public void operationIsClicked(View operation)
     {
-    
+        //Аналогично числам, лучше всего сделать через "StringBuilder":
+        answerBlock.setText(answerBlock.getText() + operators.get(answerBlock));
     }
     
     /**
@@ -52,7 +106,15 @@ public class MainActivity extends AppCompatActivity
      */
     public void deleteLastChar(View view)
     {
-    
+        if (answerBlock.getText().toString().length() == 0)
+        {
+            Toast notify = Toast.makeText(this, "Выражение отсутствует.", Toast.LENGTH_SHORT);
+            notify.show();
+
+            return;
+        }
+
+        answerBlock.setText(answerBlock.getText().subSequence(0, answerBlock.getText().length() - 1));
     }
     
     /**
@@ -63,7 +125,10 @@ public class MainActivity extends AppCompatActivity
      */
     public void clearAll(View view)
     {
-    
+        answerBlock.setText("");
+
+        Toast notify = Toast.makeText(this, "Выражение удалено.", Toast.LENGTH_SHORT);
+        notify.show();
     }
     
     /**
